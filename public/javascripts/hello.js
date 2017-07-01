47c6a8e26d5c95014e47474dcca381603101d32b
jonatantierno <jta.javato@gmail.com>
2017-07-02 01:11:30 +0200

    Fix js

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
     $('#go').click(function() {
         $.get("rhymes/" + encodeURI($("#verse").val()), function( result ) {
-            $('#rhymes').val(result);
+            $('#rhymes').html(result);
         });
         $.get("rhymes/strict/" + encodeURI($("#verse").val()), function( result ) {
-            $('#rhymes_strict').val(result);
+            $('#rhymes_strict').html(result);
         });
     });
 });
